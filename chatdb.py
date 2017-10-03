4e03c344e9c5e8fb5eb75cafefe66fc03b739056
Jia Le <tanjiale1029@gmail.com>
2017-10-03 05:08:14 +0000

    probably final

--- chatdb.py
+++ chatdb.py
@@ -1,16 +1,15 @@
 import MySQLdb
-import os
 
-class DBHelper:
-	def __init__(self, dbname = "ChatDB"):
-		self.conn = MySQLdb.connect("localhost", "root", "p@ssword", "ChatDB")
+class DBHelper: #declaring an object to be used in the main source code
+	def __init__(self, dbname = "ChatDB"): #initialise the connection to database
+		self.conn = MySQLdb.connect("localhost", "root", "p@ssword", "ChatDB") 
 		self.cursor = self.conn.cursor()
 		self.conn.set_character_set('utf8')
 		self.cursor.execute('SET NAMES utf8;')
 		self.cursor.execute('SET CHARACTER SET utf8;')
 		self.cursor.execute('SET character_set_connection=utf8;')
 
-	def add_Message(self, chat_id, messages):
+	def add_Message(self, chat_id, messages): #add message function
 		try:
 			add = "INSERT INTO content(chat_id, messages) VALUES (%s,%s)"
 			self.cursor.execute(add,(chat_id,messages))
@@ -18,7 +17,7 @@ class DBHelper:
 		except MySQLdb.Error as er:
 			print(er)
 
-	def remove_Message(self, chat_id, messages):
+	def remove_Message(self, chat_id, messages): #remove message function 
 		try:
 			delete = """DELETE FROM content WHERE chat_id = %s""" %chat_id
 			self.cursor.execute(delete)
@@ -26,11 +25,11 @@ class DBHelper:
 		except MySQLdb.Error as er:
 			print(er)
 
-	def get_Message(self, chat_id, messages):
+	def get_Message(self, chat_id, messages): #retrieving messages from the database
 		try:
 			update = "SELECT messages FROM content WHERE chat_id = %s"%chat_id
 			self.cursor.execute(update)
-			textTuple = self.cursor.fetchall()
+			textTuple = self.cursor.fetchall() 
 			return textTuple
 		except MySQLdb.Error as er:
 			print(er)
--- main1.py
+++ main1.py
@@ -1,45 +1,54 @@
+#SummariseBot for Telegram
+#CE1003 Assignment 1
+#
+#Purpose of the bot: The bot collects the chats and stored it on a localhost database according to the chat_id, and when the user asks for a summary,
+#                    the bot will once again retrieve data from the database and summarise it using textranker.
+#
+#Author: Team Instant Summarisers
+#Date Created: 20/9/2017 
+
 from summarizer import textranker
 import telepot
 from telepot.loop import MessageLoop
-import time
-import os
 from chatdb import DBHelper 
 import MySQLdb
+import time
 
 db = DBHelper()
+
+error = "Not enough text la dei, so few messages also you lazy to read?" #error code to be displayed
 
 bot = telepot.Bot("321746669:AAHNwsV42SX_KOmkYaAYW1eCNH8ydWI6fsE")
 
 def startSummarise(chat_id):
-	db.conn = MySQLdb.connect("localhost", "root", "p@ssword", "ChatDB")
-	db.cursor = db.conn.cursor()
-	textTuple = db.get_Message(chat_id,messages)
-	textString = ' ~ '.join([''.join(x) for x in textTuple])
-	textString = unicode(textString,errors="ignore")
-	if textString == "":
-		bot.sendMessage(chat_id,"Bruh you don't even have messages, don't expect any miracles to happen here.")
+	db.__init__("ChatDB")
+	textTuple = db.get_Message(chat_id,messages)  #gets messages from MySQL server
+	textString = ' ~ '.join([''.join(x) for x in textTuple]) #converts the text from tuple to string format
+	textString = unicode(textString,errors="ignore") 
+	if textString == "": 
+		bot.sendMessage(chat_id,error) #bot sends error when the database does not contain any messages
 	else:
 		summaryString = textranker(textString)
-		if summaryString == "[]": #output will be [] if there is not enough data
-			bot.sendMessage(chat_id,"Not enough text la dei, so few messages also you lazy to read?")
-			print("Summary requested from " + chat_id)
+		print("Summary requested from " + chat_id)
+		if summaryString == "[]": #output will be [] if there is not enough data, bot sends error message
+			bot.sendMessage(chat_id,error)
 		else:
-			bot.sendMessage(chat_id,"Your requested summary has arrived! \n \n "+ summaryString[3:-2])
+			bot.sendMessage(chat_id,"Your requested summary has arrived! \n \n "+ summaryString[3:-2]) #bot sends summarised texts
 
 def handle(msg):
 	content_type , chat_type ,chat_id = telepot.glance(msg)
-	if content_type == "text":
+	if content_type == "text": #checks whether user entered text
 		global messages
-		messages = msg["text"]
+		messages = msg["text"] 
 		chat_id= str(chat_id)
-		if messages == "/summarise@SummarierBot":
+		if messages == "/summarise": #checks whether user entered summarise command
 			db.__init__("ChatDB")
 			startSummarise(chat_id)
-		elif messages == "/clear@SummarierBot":
+		elif messages == "/clear": #checks whether use entered clear command
 			db.__init__("ChatDB")
 			db.remove_Message(chat_id, messages)
 			bot.sendMessage(chat_id, "Messages have been cleared.")
-		elif messages[0] != "/": #ignore messages starting with /
+		elif messages[0] != "/": #ignore messages starting with /, saves other messages into database.
 			db.__init__("ChatDB")
 			db.add_Message(chat_id,messages)
 			print("Received "+messages+" from " + chat_id)
@@ -47,4 +56,4 @@ def handle(msg):
 MessageLoop(bot,{"chat":handle}).run_as_thread()
 print ("I'm awake and currently collecting your chats")
 while 1:
-	time.sleep(10)
+	time.sleep(10) #keeps bot awake
